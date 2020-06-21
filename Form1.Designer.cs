94d51fb74f1e7956df25031aca42964fa5fb14b6
Jacob Archambault <jacob.archambault@waystar.com>
2020-06-21 09:27:46 -0400

    Add Textbox and label elements to form

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,7 +28,25 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.txtBxResult = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // txtBxResult
+            // 
+            this.txtBxResult.Location = new System.Drawing.Point(284, 290);
+            this.txtBxResult.Name = "txtBxResult";
+            this.txtBxResult.Size = new System.Drawing.Size(150, 39);
+            this.txtBxResult.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(126, 290);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 32);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Result";
             // 
             // ClubForm
             // 
@@ -36,15 +54,21 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Blue;
             this.ClientSize = new System.Drawing.Size(1040, 576);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBxResult);
             this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "ClubForm";
             this.Text = "Computer Club Outing Sign Up";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtBxResult;
+        private System.Windows.Forms.Label label1;
     }
 }
 
